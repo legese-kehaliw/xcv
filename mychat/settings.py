cf2cc678fde678b41096b7e43ff70e5978d8f7f1
legese-kehaliw <149174360+legese-kehaliw@users.noreply.github.com>
2023-11-02 07:06:18 -0700

    Update settings.py

--- mychat/settings.py
+++ mychat/settings.py
@@ -72,14 +72,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mychat.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
